41eb492f7005fa7802ce696e1d4c833080d83a71
Morten <enutie@gmail.com>
2024-06-05 09:52:26 +0200

    In the process of adding a database for cards to the api

---
+++ Aurora.Frontend/Aurora.Frontend.Server/Models/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace Aurora.Server.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+    }
+}
--- Aurora.Frontend/Aurora.Frontend.Server/Program.cs
+++ Aurora.Frontend/Aurora.Frontend.Server/Program.cs
@@ -1,6 +1,12 @@
+using Aurora.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<CardContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("CardContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
